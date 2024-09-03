515834fffb7409dcbea5f707683cd437a1825f62
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 22:52:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,25 +12,21 @@ selected_sport = st.sidebar.selectbox(label='Select sport', options=SPORTS.keys(
 
 if selected_sport is not None:
 
-  #selected_date = st.sidebar.date_input(label='Select event start time', value = 'today', min_value=datetime.date(2021, 1, 1))
-  date_range_string = date_range_picker(picker_type=PickerType.time.string_value,
-                                      start=-1, end=0, unit=Unit.days.string_value,
-                                      key='range_picker',
-                                      refresh_button={'is_show': True, 'button_name': 'Refresh last 1Days',
-                                                      'refresh_date': -1,
-                                                      'unit': Unit.days.string_value})
-  if datetime_string is not None:
-      st.write(date_range_string)
+  selected_from_date = st.sidebar.date_input(label='Select date from', value = 'today', min_value=datetime.date(2021, 1, 1), help='Specify what date you want to start searching for fixtures.')
+
+  if selected_from_date:
+
+    selected_to_date = st.sidebar.date_input(label='Select date to', value = selected_from_date + datetime.timedelta(days=3), max_value=selected_from_date + datetime.timedelta(days=10), help='Specify what date you want to end your search. Please note that a maximum range of 10 days is allowed.')
+    
+    if selected_to_date is not None:
   
-      if selected_date is not None:
-    
-        events = db.get_fixtures(sport_id=SPORTS[selected_sport], starts=selected_date)
-    
-        event_options = dict()
-        for index, row in events.iterrows():
-          event_options.update({row['event_id']: f"{row['starts']} {row['league_name'].upper()} {row['runner_home']} - {row['runner_away']}"})
-        
-        selected_event = st.sidebar.selectbox(label='Select event', options=event_options.keys(), index=None, format_func=lambda x: event_options.get(x), placeholder='Start typing...')
-        
-        st.sidebar.write(selected_event)
+      events = db.get_fixtures(sport_id=SPORTS[selected_sport], starts=selected_date)
+  
+      event_options = dict()
+      for index, row in events.iterrows():
+        event_options.update({row['event_id']: f"{row['starts']} {row['league_name'].upper()} {row['runner_home']} - {row['runner_away']}"})
+      
+      selected_event = st.sidebar.selectbox(label='Select event', options=event_options.keys(), index=None, format_func=lambda x: event_options.get(x), placeholder='Start typing...')
+      
+      st.sidebar.write(selected_event)
     
