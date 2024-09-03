9e375812825488b300a0b222be849f2012737758
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 15:34:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ st.set_page_config(layout="wide")
 # Add a bet
 st.write('Add a bet')
 unique_sports = db.get_sports()
+st.write(unique_sports)
 selected_sport = st.selectbox(label='Sports', options=sorted(unique_sports))
 
