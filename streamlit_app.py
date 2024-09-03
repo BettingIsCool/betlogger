71adc6f22f6f669e6476738de1d0bb9716f0d6c4
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 21:19:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,12 @@ selected_sport = st.sidebar.selectbox(label='Select sport', options=SPORTS.keys(
 
 if selected_sport is not None:
 
-  selected_date = st.sidebar.date_input(label='Select date', value = 'today', min_value=datetime.date(2021, 1, 1), help='Event start time (CET)')
+  selected_date = st.sidebar.date_input(label='Select date', value = 'today', min_value=datetime.date(2021, 1, 1), help='Event start date')
 
   if selected_date is not None:
 
-    st.sidebar.write(selected_date)
-    leagues = db.get_leagues(sport_id=SPORTS[selected_sport])
+    leagues = db.get_fixtures(sport_id=SPORTS[selected_sport], starts=selected_date)
     selected_league = st.sidebar.selectbox(label='Select league', options=leagues['league_name'], index=None, placeholder='Start typing...')
     
     if selected_league is not None:
       st.sidebar.write(leagues[leagues['league_name'] == selected_league]['league_id'])
-
