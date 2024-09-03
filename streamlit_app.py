e5336107c4e6d50c699d760dbb9e387d728ebd46
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 19:32:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ from config import SPORTS
 st.sidebar.write('Add a bet')
 selected_sport = st.sidebar.selectbox(label='Sports', options=SPORTS.keys())
 
-st.write(selected_sport)
-
 leagues = db.get_leagues(sport_id=SPORTS[selected_sport])
-st.write(leagues)
+st.sidebar.write(leagues['league_name'])
 
