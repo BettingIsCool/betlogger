0977ba0e5bef4ceacfdde4852de6c81b68e0a647
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 19:24:22 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -8,4 +8,4 @@ conn = st.connection('pinnacle', type='sql')
 @st.cache_data(ttl=10)
 def get_leagues(sport_id: int):
 
-  return conn.query(f"SELECT league_id, league_name FROM {TABLE_LEAGUES} WHERE sport_id = {sport_id}", ttl=600)['sport_name'].tolist()
+  return conn.query(f"SELECT league_id, league_name FROM {TABLE_LEAGUES} WHERE sport_id = {sport_id}", ttl=600).tolist()
