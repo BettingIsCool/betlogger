27e0d4e0694c1e901848c21d6b19f61503a60219
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 21:16:09 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -9,3 +9,9 @@ conn = st.connection('pinnacle', type='sql')
 def get_leagues(sport_id: int):
 
   return conn.query(f"SELECT league_id, league_name FROM {TABLE_LEAGUES} WHERE sport_id = {sport_id}", ttl=600)
+
+
+@st.cache_data(ttl=10)
+def get_fixtures(sport_id: int, starts: datetime):
+
+  return conn.query(f"SELECT DISTINCT(league_id), league_name FROM {TABLE_FIXTURES} WHERE sport_id = {sport_id} and DATE(starts) = '{starts.strftime('%Y-%m-%d')}'", ttl=600)
