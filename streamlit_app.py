40427aca48306eed167498d56be1b534ece4ea85
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-04 01:14:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if selected_sport is not None:
       if selected_event is not None:
 
         odds = db.get_odds(event_id=selected_event)
-        selected_period = st.sidebar.selectbox(label='Select period', options=sorted(odds['period']), index=0)
+        selected_period = st.sidebar.selectbox(label='Select period', options=sorted(odds.period.unique()), index=0)
 
         #st.sidebar.write(odds)
     
