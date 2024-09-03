05ffd5c2928dad22f2f0cbced62c6930012fdb6a
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 14:40:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import db_pinnacle as db
 
-# Switch to wide-mode for better view
-st.set_page_config(layout="wide")
-
 # Add a bet
 unique_sports = db.get_sports()
 selected_sports = st.selectbox(label='Sports', options=sorted(unique_sports))
