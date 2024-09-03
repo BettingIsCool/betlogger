3c641b841ff7c8d9d3c18bf667b2c141e4aa4433
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 23:46:51 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -16,3 +16,9 @@ def get_leagues(sport_id: int):
 def get_fixtures(sport_id: int, date_from: datetime, date_to: datetime):
 
   return conn.query(f"SELECT DISTINCT(f.event_id), f.league_id, f.league_name, f.starts, f.runner_home, f.runner_away FROM {TABLE_FIXTURES} f, {TABLE_ODDS} o, {TABLE_RESULTS} r WHERE f.sport_id = {sport_id} AND DATE(f.starts) >= '{date_from.strftime('%Y-%m-%d')}' AND DATE(f.starts) <= '{date_to.strftime('%Y-%m-%d')}' AND o.event_id = f.event_id AND r.event_id = f.event_id ORDER BY f.starts", ttl=600)
+
+
+@st.cache_data(ttl=10)
+def get_odds(event_id: int):
+
+  return conn.query(f"SELECT period, market, line FROM {TABLE_ODDS} WHERE event_id = {event_id}", ttl=600)
