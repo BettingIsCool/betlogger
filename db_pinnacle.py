432224ce571b910bc9098c70717f5043269944e8
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-03 14:24:29 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from sqlalchemy import text
-from config import TABLE_FIXTURES, TABLE_ODDS, TABLE_RESULTS
+from config import TABLE_SPORTS, TABLE_LEAGUES, TABLE_FIXTURES, TABLE_ODDS, TABLE_RESULTS, TABLE_BETS, TABLE_USERS
 
 conn = st.connection('imr', type='sql')
 
@@ -8,4 +8,4 @@ conn = st.connection('imr', type='sql')
 @st.cache_data(ttl=10)
 def get_sports():
 
-  return conn.query(f"SELECT DISTINCT(sport_name) FROM {TABLE_FIXTURES}", ttl=600)['sport_name'].tolist()
+  return conn.query(f"SELECT DISTINCT(sport_name) FROM {TABLE_SPORTS}", ttl=600)['sport_name'].tolist()
