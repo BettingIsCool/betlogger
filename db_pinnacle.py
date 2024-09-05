7753a71c09410b1bce40cf6e58b0329d7c0f6b6c
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-05 11:41:39 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -28,3 +28,12 @@ def get_odds(event_id: int):
 def get_users():
 
   return conn.query(f"SELECT name, username, password FROM {TABLE_USERS}", ttl=600).to_dict('records')
+
+
+def append_bet(data: dict):
+
+  query = f"INSERT INTO {TABLE_BETS} (user, tag, starts, sport_id, sport_name, league_id, league_name, event_id, runner_home, runner_away, market, period, side, raw_line, odds, stake) VALUES(:user, :tag, :starts, :sport_id, :sport_name, :league_id, :league_name, :event_id, :runner_home, :runner_away, :market, :period, :side, :raw_line, :odds, :stake)"
+ 
+  with conn.session as session:
+    session.execute(text(query), params = dict(user = data['user'], tag = data['tag'], starts = data['starts'], sport_id = data['sport_id'], sport_name = data['sport_name'], league_id = data['league_id'], league_name = data['league_name'], event_id = data['event_id'], runner_home = data['runner_home'], runner_away = data['runner_away'], market = data['market'], period = data['period'], side = data['side'], raw_line = data['raw_line'], odds = data['odds'], stake = data['stake']))
+    session.commit()  
