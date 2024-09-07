8142dc4d415f3ffee8b00af92c1107a562b64e8e
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-07 14:48:49 +0200

    Update db_pinnacle.py

--- db_pinnacle.py
+++ db_pinnacle.py
@@ -30,7 +30,7 @@ def append_bet(data: dict):
   query = f"INSERT INTO {TABLE_BETS} (user, tag, starts, sport_id, sport_name, league_id, league_name, event_id, runner_home, runner_away, market, period, side, raw_line, odds, stake, bookmaker) VALUES(:user, :tag, :starts, :sport_id, :sport_name, :league_id, :league_name, :event_id, :runner_home, :runner_away, :market, :period, :side, :raw_line, :odds, :stake, :bookmaker)"
  
   with conn.session as session:
-    session.execute(text(query), params = dict(user = data['user'], tag = data['tag'], starts = data['starts'], sport_id = data['sport_id'], sport_name = data['sport_name'], league_id = data['league_id'], league_name = data['league_name'], event_id = data['event_id'], runner_home = data['runner_home'], runner_away = data['runner_away'], market = data['market'], period = data['period'], side = data['side'], raw_line = data['raw_line'], odds = data['odds'], stake = data['bookmaker'], stake = data['bookmaker']))
+    session.execute(text(query), params = dict(user = data['user'], tag = data['tag'], starts = data['starts'], sport_id = data['sport_id'], sport_name = data['sport_name'], league_id = data['league_id'], league_name = data['league_name'], event_id = data['event_id'], runner_home = data['runner_home'], runner_away = data['runner_away'], market = data['market'], period = data['period'], side = data['side'], raw_line = data['raw_line'], odds = data['odds'], stake = data['stake'], bookmaker = data['bookmaker']))
     session.commit()
 
 
