a67bace03dd8d518c9a628c2ce859c626a60b80d
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-07 20:15:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,8 +171,10 @@ if authentication_status:
         selected_tags = [f"'{s}'" for s in selected_tags]
         selected_tags = f"({','.join(selected_tags)})"
         
-        bets = db.get_bets(username=username, sports=selected_sports, leagues=selected_leagues, bookmakers=selected_bookmakers, tags=selected_tags)
-        bets_df = pd.DataFrame(bets)
-        st.write(bets_df)                
+        if selected_tags is not None:
+        
+          bets = db.get_bets(username=username, sports=selected_sports, leagues=selected_leagues, bookmakers=selected_bookmakers, tags=selected_tags)
+          bets_df = pd.DataFrame(bets)
+          st.write(bets_df)                
                 
     
