d3d63d618d1671e1f517a7acfc75e36b10901e88
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-07 22:50:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -200,6 +200,7 @@ if authentication_status:
             selected_date_to = st.sidebar.date_input(label='Select end date', value = max(user_unique_starts), min_value=min(user_unique_starts), max_value=max(user_unique_starts), help='Specify the end date for analyzing your bets (= those in the table on the right side). You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
             
             bets = db.get_bets(username=username, sports=selected_sports, leagues=selected_leagues, bookmakers=selected_bookmakers, tags=selected_tags, date_from=selected_date_from, date_to=selected_date_to)
+            st.write(bets)
             
             bets_df = pd.DataFrame(data=bets)
             st.dataframe(data=bets_df, hide_index=True)                
