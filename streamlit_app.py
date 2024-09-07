11fdba1ff715b70affe0da1a0fc6e66f5fd6ce81
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-07 20:51:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import datetime
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Betlogger by BettingIsCool", page_icon="🦈", layout="wide", initial_sidebar_state="expanded")
 
 import db_pinnacle as db
 import streamlit_authenticator as stauth
