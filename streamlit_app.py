684b456d96d975af30c3a6603d9b9946db30d695
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-08 12:53:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ if authentication_status:
                         data.update({'side_name': side_options[selected_side]})
                         data.update({'side': selected_side})
                         data.update({'raw_line': selected_line})
-                        #data.update({'line': line_options[selected_line]}) if selected_line is not None else data.update({'line': None})
+                        data.update({'line': line_options[selected_line]}) if selected_line is not None else data.update({'line': None})
                         #data.update({'odds': odds})
                         #data.update({'stake': stake})
                         #data.update({'bookmaker': book})
