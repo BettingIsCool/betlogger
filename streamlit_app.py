b29dcdf0f867db88c1be0ef57e70edf0428818b0
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-08 20:09:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -248,10 +248,10 @@ if authentication_status:
             bets_df = bets_df[['DEL', 'TAG', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'PERIOD', 'SIDE', 'LINE', 'ODDS', 'STAKE', 'BOOK', 'STATUS', 'SH', 'SA', 'P/L', 'CLS', 'CLS_TRUE', 'CLS_LIMIT', 'EXP_WIN', 'CLV%', 'BET_ADDED', 'ID']]
 
             # Apply font & background colors to cells, apply number formatting
-            styled_df = bets_df.style.applymap(color_cells, subset=['STATUS', 'P/L', 'EXP_WIN', 'CLV%']).format({'LINE': '{:+g}'.format, 'ODDS': '{:,.3f}'.format, 'STAKE': '{}'.format, 'CLS_LIMIT': '{0:g}'.format})
+            #styled_df = bets_df.style.applymap(color_cells, subset=['STATUS', 'P/L', 'EXP_WIN', 'CLV%']).format({'LINE': '{:+g}'.format, 'ODDS': '{:,.3f}'.format, 'STAKE': '{}'.format, 'CLS_LIMIT': '{0:g}'.format})
 
             
-            df = st.data_editor(styled_df, column_config={"DEL": st.column_config.CheckboxColumn("DEL", help="Select if you want to delete this bet!", default=False)}, disabled=['TAG', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'PERIOD', 'SIDE', 'LINE', 'ODDS', 'STAKE', 'BOOK', 'STATUS', 'SH', 'SA', 'P/L', 'CLS', 'CLS_TRUE', 'CLS_LIMIT', 'EXP_WIN', 'CLV%', 'BET_ADDED', 'ID'], hide_index=True)
+            df = st.data_editor(bets_df, column_config={"DEL": st.column_config.CheckboxColumn("DEL", help="Select if you want to delete this bet!", default=False)}, disabled=['TAG', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'PERIOD', 'SIDE', 'LINE', 'ODDS', 'STAKE', 'BOOK', 'STATUS', 'SH', 'SA', 'P/L', 'CLS', 'CLS_TRUE', 'CLS_LIMIT', 'EXP_WIN', 'CLV%', 'BET_ADDED', 'ID'], hide_index=True)
   
   #delete_bets = st.button('Delete selected bet(s)')
   bets_to_be_deleted = df.loc[(df['DEL'] == True), 'ID'].tolist()
