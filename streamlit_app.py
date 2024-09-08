c4773059b2ecd2b5bda942a115219454b96dee03
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-08 23:41:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -264,11 +264,13 @@ if authentication_status:
             df = st.data_editor(styled_df, column_config={"DEL": st.column_config.CheckboxColumn("DEL", help="Select if you want to delete this bet!", default=False)}, disabled=['TAG', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'PERIOD', 'SIDE', 'LINE', 'ODDS', 'STAKE', 'BOOK', 'ST', 'SH', 'SA', 'P/L', 'CLS', 'CLS_TRUE', 'CLS_LIMIT', 'EXP_WIN', 'CLV', 'BET_ADDED', 'ID'], hide_index=True)
 
             chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+            
+            bets_to_be_deleted = df.loc[(df['DEL'] == True), 'ID'].tolist()
+            st.button('Delete selected bet(s)', on_click=delete_bets, args=(bets_to_be_deleted,))
+            
             st.line_chart(chart_data)
-  #delete_bets = st.button('Delete selected bet(s)')
-  bets_to_be_deleted = df.loc[(df['DEL'] == True), 'ID'].tolist()
 
-  st.button('Delete selected bet(s)', on_click=delete_bets, args=(bets_to_be_deleted,))
+  
 
   
                 
