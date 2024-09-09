503037cf98268def2d82f033632c9b4812622e98
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-09 22:59:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -308,9 +308,9 @@ if authentication_status:
         dates.append(row['STARTS'])
 
 
-    chart_data = pd.DataFrame({"Dates": dates, "Actual P/L": cum_profit, "CLV": cum_clv,})
+    chart_data = pd.DataFrame({"Dates": dates, "Actual P/L": cum_profit, "CLV": cum_clv}, columns=["a", "b", "c"])
 
-    st.line_chart(chart_data, x="Dates", y="Actual P/L", color="CLV")
+    st.line_chart(chart_data)
     #chart_data = pd.DataFrame(np.array([dates, cum_profit, cum_clv]), columns=["Date", "Actual P/L", "CLV"])
     #st.line_chart(chart_data, x="Date", y=["Actual P/L", "CLV"], color=["#FF0000", "#0000FF"])
 
