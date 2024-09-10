44f9732520475f286f08715c2e0dd106bacd7025
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-10 17:34:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,14 +40,14 @@ if username not in users:
   db.append_user(data={'username': username})
   users.add(username)
 
-
-# ONLY AFTER THE AUTHENTICATION + SUBSCRIPTION, THE USER WILL SEE THIS ⤵
-# The email and subscription status is stored in session state.
-st.write(f"Subscription Status: {st.session_state.user_subscribed}")
-st.write("🎉 Yay! You're all set and subscribed! 🎉")
-st.write(f'By the way, your email is: {st.session_state.email}')
-
 bets_to_be_deleted, df = set(), set()
+
+if not df:
+  # ONLY AFTER THE AUTHENTICATION + SUBSCRIPTION, THE USER WILL SEE THIS ⤵
+  # The email and subscription status is stored in session state.
+  st.write(f"Subscription Status: {st.session_state.user_subscribed}")
+  st.write("🎉 Yay! You're all set and subscribed! 🎉")
+  st.write(f'By the way, your email is: {st.session_state.email}')
 
 # Start of helper functions 
 
